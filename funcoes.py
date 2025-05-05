f68feed38ac8fe0f97ef1406e1661165a377fe57
LuisF-CRosa <luisfcr1@al.insper.edu.br>
2025-05-05 09:40:33 -0300

    Exercicio 5 - Luís Felipe

--- funcoes.py
+++ funcoes.py
@@ -1,22 +1,21 @@
-#Exercicos 1,2,3 = João Pedro
-
+#Exercicos 1 = João Pedro:
 def rolar_dados(quant_dado):
     import random 
     list_dado=[]
     for i in range(0,quant_dado):
         list_dado.append(random.randint(1,6))
     return list_dado
+#Exercicos 2 = João Pedro:
 def guardar_dado(rolados,guardados,dado):
     guardados.append(rolados[dado])
     del rolados[dado]
     return [rolados,guardados]
+#Exercicos 3 = João Pedro:
 def remover_dado(rolados,guardados,dado):
     rolados.append(guardados[dado])
     del guardados[dado]
     return [rolados,guardados]
-
-#Exercicos 4 = Luís Felipe
-
+#Exercicos 4 = Luís Felipe:
 def calcula_pontos_regra_simples(valor_dado):
     calculo={}
     for chave in range(1,7):
@@ -24,3 +23,9 @@ def calcula_pontos_regra_simples(valor_dado):
     for valor in valor_dado:
         calculo[valor]=calculo[valor]+valor
     return calculo
+#Exercicos 5 = Luís Felipe:
+def calcula_pontos_soma(face_dado):
+    soma=0
+    for i in face_dado:
+        soma+=i
+    return soma

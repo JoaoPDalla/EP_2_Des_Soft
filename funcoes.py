f70caf9022dc1924bb8d16c72db4ccc1e57dcac9
LuisF-CRosa <luisfcr1@al.insper.edu.br>
2025-05-05 09:32:23 -0300

    Exercicio 4 - Luís Felipe

--- funcoes.py
+++ funcoes.py
@@ -1,3 +1,5 @@
+#Exercicos 1,2,3 = João Pedro
+
 def rolar_dados(quant_dado):
     import random 
     list_dado=[]
@@ -12,3 +14,13 @@ def remover_dado(rolados,guardados,dado):
     rolados.append(guardados[dado])
     del guardados[dado]
     return [rolados,guardados]
+
+#Exercicos 4 = Luís Felipe
+
+def calcula_pontos_regra_simples(valor_dado):
+    calculo={}
+    for chave in range(1,7):
+        calculo[chave]=0
+    for valor in valor_dado:
+        calculo[valor]=calculo[valor]+valor
+    return calculo

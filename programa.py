e622162754e3821bd820cc176cb076a49cdfd4c4
Joao <joaopdbm@al.insper.edu.br>
2025-05-06 21:15:52 -0300

    exercicio 13-quase terminado

--- programa.py
+++ programa.py
@@ -30,9 +30,9 @@ for t in range(0,12):
     print('Dados rolados: {0}'.format(rolados))
     print('Dados guardados: {0}'.format(guardados))
     print("Digite 1 para guardar um dado, 2 para remover um dado, 3 para rerrolar, 4 para ver a cartela ou 0 para marcar a pontuação:")
-    opc=int(input())
-    while opc!=0:
-        if opc==1:
+    opc=input()
+    while opc!='0':
+        if opc=='1':
             print("Digite o índice do dado a ser guardado (0 a 4):")
             g=int(input())
             while g>len(rolados):
@@ -41,7 +41,7 @@ for t in range(0,12):
             gi=fc.guardar_dado(rolados,guardados,g)
             rolados=gi[0]
             guardados=gi[1]
-        elif opc==2:
+        elif opc=='2':
             print("Digite o índice do dado a ser removido (0 a 4):")
             r=int(input())
             while r>len(guardados):
@@ -50,20 +50,21 @@ for t in range(0,12):
             ri=fc.remover_dado(rolados,guardados,r)
             rolados=ri[0]
             guardados=ri[1]
-        elif opc==3:
+        elif opc=='3':
             if uso_r<2:
                 rolados=fc.rolar_dados(len(rolados))
                 uso_r=uso_r+1
             else:
                 print("Você já usou todas as rerrolagens.")
-        elif opc==4:
+        elif opc=='4':
             fc.imprime_cartela(cartela_de_pontos)
         else:
-            print("Combinação inválida. Tente novamente.")
-        print('Dados rolados: {0}'.format(rolados))
-        print('Dados guardados: {0}'.format(guardados))
-        print("Digite 1 para guardar um dado, 2 para remover um dado, 3 para rerrolar, 4 para ver a cartela ou 0 para marcar a pontuação:")
-        opc=int(input())
+            print("Opção inválida. Tente novamente.")
+        if opc=='1' or opc=='2' or opc=='3' or opc=='4':
+            print('Dados rolados: {0}'.format(rolados))
+            print('Dados guardados: {0}'.format(guardados))
+            print("Digite 1 para guardar um dado, 2 para remover um dado, 3 para rerrolar, 4 para ver a cartela ou 0 para marcar a pontuação:")
+        opc=input()
     print("Digite a combinação desejada:")
     j=input()
     for i in guardados:
